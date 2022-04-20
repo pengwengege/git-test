6236d9a7d604f6c5d23c61820dfb7ad46a515472
pw <2738558783@qq.com>
2022-04-20 16:05:55 +0800

    first commit

---
+++ src/main/java/com/pw/GitTest.java
@@ -0,0 +1,8 @@
+package com.pw;
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+        System.out.println("hello git2");
+    }
+}
